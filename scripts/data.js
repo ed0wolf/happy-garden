d270928b0d8cb7ca08dfb68c2be691e2b2345e88
Ed0wolf <Ed0wolf@gmail.com>
2014-12-21 13:11:29 +0000

    Initial commit

---
+++ scripts/data.js
@@ -0,0 +1,35 @@
+var menuData = [
+    {
+        name: 'Starters',
+        dishes: [
+            {
+                number: '1',
+                name: 'Hoisin Duck',
+                description: 'Duck in Hoisin Sauce',
+                price: '£5.00'
+            }
+        ]
+    },
+    {
+        name: 'Mains',
+        dishes: [
+            {
+                number: '2',
+                name: 'Fish and Chips',
+                description: 'Duck in Hoisin Sauce',
+                price: '£10.00'
+            }
+        ]
+    },
+    {
+        name: 'Puds',
+        dishes: [
+            {
+                number: '3',
+                name: 'Chocolate Cake',
+                description: 'Duck in Hoisin Sauce',
+                price: '£4.00'
+            }
+        ]
+    }
+];
---
+++ scripts/index.js
@@ -0,0 +1,130 @@
+/*globals console: true, menuData: true  */
+var $ = window.jQuery;
+
+var Dish = (function () {
+    'use strict';
+    function Dish(dishData) {
+        this.number = dishData.number;
+        this.name = dishData.name;
+        this.description = dishData.description;
+        this.price = dishData.price;
+    }
+        
+    function numberAsDom(number) {
+        return '<span class="dish-number">' + number + '<span>';
+    }
+    
+    function nameAsDom(name) {
+        return ' <span class="dish-name">' + name + '<span>';
+    }
+    
+    function priceAsDom(price) {
+        return ' <span class="dish-price">' + price + '<span>';
+    }
+    
+    
+    Dish.prototype.asDom = function () {
+        var dishDiv = $('<div>');
+        dishDiv.append(numberAsDom(this.number));
+        dishDiv.append(nameAsDom(this.name));
+        dishDiv.append(priceAsDom(this.price));
+        return dishDiv;
+    };
+    
+    return Dish;
+}());
+
+var Section = (function () {
+    'use strict';
+    function Section(sectionData) {
+        this.name = sectionData.name;
+        this.dishes = sectionData.dishes.map(function (dish) {
+            return new Dish(dish);
+        });
+    }
+    
+    function nameAsDom(name) {
+        return '<h2>' + name + '</h2>';
+    }
+    
+    Section.prototype.asDom = function () {
+        var sectionEle = $('<div>');
+        sectionEle.append(nameAsDom(this.name));
+        this.dishes.forEach(function (dish) {
+            sectionEle.append(dish.asDom());
+        });
+        return sectionEle;
+    };
+    
+    return Section;
+}());
+
+var Search = (function () {
+    'use strict';
+    function Search(sections, searchCallback) {
+        this.sections = sections;
+        this.searchCallback = searchCallback;
+    }
+    
+    function filterDishes(searchValue, sections) {
+        return sections.map(function (section) {
+            var filteredDishes = section.dishes.filter(function (dish) {
+                return new RegExp(searchValue, ['i']).test(dish.name);
+            });
+            return new Section({ name: section.name, dishes: filteredDishes });
+        });
+    }
+    
+    Search.prototype.asDom = function () {
+        var me = this,
+            searchBox = $('<input id="menuzzy-search">');
+        searchBox.keyup(function (e) {
+            var searchValue = this.value;
+            me.searchCallback(filterDishes(searchValue, me.sections));
+        });
+        return searchBox;
+    };
+    
+    return Search;
+}());
+
+var Menuzzy = (function () {
+    'use strict';
+    function Menuzzy(menuData) {
+        this.sections = menuData.map(function (sectionData) {
+            return new Section(sectionData);
+        });
+    }
+    
+    function createSectionsDom(sections) {
+        var sectionsContainer = $('<div id="menuzzy-sections">');
+        sections.forEach(function (section) {
+            sectionsContainer.append(section.asDom());
+        });
+        return sectionsContainer;
+    }
+    
+    Menuzzy.prototype.asDom = function () {
+        var menuDiv = $('<div>'),
+            searchCallback = function (sections) {
+                var newSectionsDom = createSectionsDom(sections);
+                $('#menuzzy-sections').replaceWith(newSectionsDom);
+            },
+            search = new Search(this.sections, searchCallback);
+        
+        menuDiv.append(search.asDom());
+        menuDiv.append(createSectionsDom(this.sections));
+        
+        return menuDiv;
+    };
+    
+    return Menuzzy;
+}());
+
+var menu = new Menuzzy(menuData);
+
+$(function () {
+    'use strict';
+    var menuDom = menu.asDom();
+    $('#menuzzy-container').append(menuDom);
+});
